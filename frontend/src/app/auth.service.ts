985a2cae03bbe7cc95b7193ef9e0438a46b93c6d
Abdul Rehman <mehars.6925@gmail.com>
2021-11-12 06:44:07 +0500

    Update auth.service.ts

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private serverURI: string = 'https://mean-secure-app.herokuapp.com/';
+  private serverURI: string = 'https://mean-secure-app.herokuapp.com';
   
   constructor(private http: HttpClient) { }
 
